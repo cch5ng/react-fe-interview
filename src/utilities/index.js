54729a7c081c7d45663dd8ae465376e7c9a92219
cch5ng <cch5ng@gmail.com>
2017-02-20 03:39:09 -0800

    fix: faulty logic in random() call; max was off by one bc of array index

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,8 +1,37 @@
 // src/utilities.index.js
-// helper functions like converting source html to json
+import h5bp_interview from '../utilities/h5bp_interview.json';
 
-import { html2json } from 'html2json';
+export function getRandomIndexList(listId, count) {
+	var idxList = [];
+	var curCount = count;
+	var max = getTotalQuestions(listId);
 
-export var h5bp_interview = html2json('h5bp_interview.html');
+	while (curCount > 0) {
+		var randomIdx = getRandomInt(0, max);
+		if (idxList.indexOf(randomIdx) === -1) {
+			idxList.push(randomIdx);
+			curCount -= 1;
+		}
+	}
 
-console.log(h5bp_interview.json);
+	return idxList;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getTotalQuestions(listId) {
+	var total;
+
+	for (var i = 0; i < h5bp_interview.questions.length; i++) {
+		if (h5bp_interview.questions[i].id === listId) {
+			total = h5bp_interview.questions[i].question_list.length;
+			break;
+		}
+	}
+
+	return total;
+}

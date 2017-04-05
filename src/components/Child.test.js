59eaae68519de5d0b7ef4410652af15b62e97544
cch5ng <cch5ng@gmail.com>
2017-04-05 14:20:51 -0700

    fix: #6; cleanup Child test

--- src/components/Child.test.js
+++ src/components/Child.test.js
@@ -6,15 +6,9 @@ import { MemoryRouter } from 'react-router';
 import Child from './Child';
 
 test('Child renders', () => {
-  const props = {
-    params: {
-      url: 'defaultUrl'
-    }
-  };
-
   const child = shallow(
-  	<MemoryRouter initialEntries={[ '/child/defaultUrl' ]}>
-	    <Child {...props} />
+    <MemoryRouter initialEntries={[ '/child/defaultUrl' ]}>
+      <Child />
     </MemoryRouter>
   );
 
